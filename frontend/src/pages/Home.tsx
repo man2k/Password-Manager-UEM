406cba697ee2a718f7c66e169e0ece0cce0d5a3a
man2k <mankarmakar4@gmail.com>
2023-11-07 22:23:19 +0530

    first commit

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+const Home = () => {
+  return (
+    <main className="bg-black min-h-screen min-w-full text-white">
+      <div className="md:pt-40 p-20 flex justify-center">
+        <h1 className="h-52 w-40 font-mono">
+          <span className="text-indigo-300 text-3xl">
+            <span className="text-sky-400 text-5xl">P</span>assword{" "}
+            <span>
+              <span className="text-sky-400 text-5xl">M</span>anager{" "}
+              <span className="text-sky-400 text-5xl">UEM</span>
+            </span>
+          </span>{" "}
+        </h1>
+        <h2>
+          <button className="mt-12 border-[1px] rounded-md p-2 ml-6">
+            <Link to="/login">Get Started</Link>
+          </button>
+        </h2>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
